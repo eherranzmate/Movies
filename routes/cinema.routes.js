b4219e622445ee02e00714bf853aa0530aa389e3
eherranzmate <eherranzmate@gmail.com>
2023-11-30 14:59:20 +0100

    second commit

---
+++ routes/cinema.routes.js
@@ -0,0 +1,89 @@
+const mongoose = require ('mongoose');
+const express = require('express');
+const router = express.Router();
+const Cinema = require('../models/Cinema');
+const Movie = require('../models/Movie');
+
+router.post('/create', async (req, res, next) =>{
+    try{
+        const newCinema = new Cinema({
+            name: req.body.name,
+            location: req.body.location,
+            movies: []
+        });
+        const createdCinema = await newCinema.save();
+        return res.status(201).json(createdCinema)
+
+    } catch(err){
+        next(err)     
+    }
+});
+
+router.get('/', async (req, res, next) => {
+    try{
+        const cinemas = await Cinema.find().populate('movies');
+        return res.status(201).json(cinemas)
+    } catch(err) {
+        next(err)
+
+    }
+
+})
+
+// añadir películas al cine
+router.put('/:id/add-movies', async (req, res, next) => {
+    try{
+        const cinemaId = req.params.id;
+        const movieIds = req.body.movieIds;
+        const updatedCinema = await Cinema.findByIdAndUpdate(cinemaId, {
+            $push: {movies: {$each: movieIds}},
+        });
+
+        return res.status(200).json(updatedCinema);
+
+    } catch(err) {
+        next(err)
+    }        
+});
+
+// meter en un cine, todas las películas de un determinado género
+router.put('/:id/add-movies-by-genre', async (req, res, next) => {
+    try{
+        const cinemaId = req.params.id;
+        const genre = req.body.genre;
+        const moviesToAdd = await Movie.find({genre: genre});
+        const updatedCinema = await Cinema.findByIdAndUpdate(cinemaId, {
+            $push: {movies: {$each: moviesToAdd}} //$addToSet para meter las películas sin repetirlas.
+        })
+
+        return res.status(200).json(updatedCinema);
+
+    } catch(err) {
+        next(err)
+    }        
+});
+
+router.put("/:id", async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const characterModify = new Character(req.body);
+        characterModify._id = id
+        const characterUpdated = await Character.findByIdAndUpdate(id, characterModify);
+        if(!characterUpdated) {
+            let error = new Error('Personaje no encontrado');
+            error.status = 404;
+            throw error;
+        } else {
+            res.status(200).json(characterUpdated);
+        }
+        
+    } catch(err) {
+        next(err)
+    }
+});
+
+module.exports = router
+
+//655b37c789bfcf16976d09cd
+
+//6565d2d7d5be6344f44837a2
---
+++ routes/movies.routes.js
@@ -0,0 +1,124 @@
+const mongoose = require ('mongoose');
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/Movie');
+
+router.get("/", async (req, res)=> {
+    try {
+      const movies = await Movie.find();
+      return res.status(200).json(movies);
+    } catch {
+      return res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+	const id = req.params.id;
+	try {
+		const moviesById = await Movie.findById(id);
+		if (moviesById) {
+			return res.status(200).json(moviesById);
+		} else {
+			return res.status(404).json('No se ha encontrado');
+		}
+	} catch (err) {
+		return res.status(500).json(err);
+	}
+});
+
+router.get("/title/:title", async (req, res)=> {
+    const title = req.params.title;
+    try {
+      const moviesByTitle = await Movie.find({title: title})
+        if (!moviesByTitle.length) {
+            return res.status(400).json('No existe')
+        }
+        return res.status(200).json(moviesByTitle);
+
+    } 
+    catch(err) {
+      return res.status(500).json(err);
+    }
+});
+
+router.get("/genre/:genre", async (req, res)=> {
+    const genre = req.params.genre;
+    try {
+      const moviesByGenre = await Movie.find({genre: genre})
+        if (!moviesByGenre.length) {
+            return res.status(400).json('No existe')
+        }
+        return res.status(200).json(moviesByGenre);
+
+    } 
+    catch(err) {
+      return res.status(500).json(err);
+    }
+});
+
+router.get('/year/:year', async (req, res) => {
+    const {year} = req.params; 
+    try {
+      const moviesByYear = await Movie.find({year: {$gt: year}});
+      if (!moviesByYear.length) {
+        return res.status(404).json('No existe')
+      }
+        
+      return res.status(200).json(moviesByYear);
+    } 
+    catch(err) {
+      return res.status(500).json(err);
+    }
+  });
+
+  router.post('/create', async (req, res, next) =>{
+    try{
+        const newMovie = new Movie({
+          title: req.body.title,
+          director: req.body.director,
+          year: req.body.year ,
+          genre: req.body.genre
+        });
+        const createdMovie = await newMovie.save();
+        return res.status(201).json(createdMovie)
+
+    } catch(err){
+        next(err)     
+    }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+      const id = req.params.id;
+      const movieModify = new Movie(req.body);
+      movieModify._id = id
+      const movieUpdated = await Movie.findByIdAndUpdate(id, movieModify);
+      if(!movieUpdated) {
+          let error = new Error('Película no encontrada');
+          error.status = 404;
+          throw error;
+      } else {
+          res.status(200).json(movieUpdated);
+      }
+      
+  } catch(err) {
+      next(err)
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+      const id = req.params.id;
+      const deletedMovie = await Movie.findByIdAndDelete(id);
+      if(deletedMovie) res.status(200).json(deletedMovie);
+      else {
+          let error = new Error('Película no encontrada');
+          error.status = 404;
+          throw error;
+      }
+  } catch(err){
+      next(err)
+  }
+});
+
+module.exports = router;
